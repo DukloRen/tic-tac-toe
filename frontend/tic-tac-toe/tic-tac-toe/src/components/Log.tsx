e722c0ed0aba92e3ab668facb8f4f25bd413c21b
DukloRen <benedekbarany@gmail.com>
2024-03-14 11:14:32 +0100

    Almost complete

---
+++ frontend/tic-tac-toe/tic-tac-toe/src/components/Log.tsx
@@ -0,0 +1,12 @@
+import GameTurn from "../classes/GameTurn";
+
+interface LogProps{
+    gameTurns: GameTurn[];
+}
+export default function Log(props: LogProps) {
+    return (
+        <li>
+            {props.gameTurns.map(turn => <p>{turn.player} selected ({turn.square.row}, {turn.square.column})</p>)}
+        </li>
+    )
+}
